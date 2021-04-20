bb428be9ba39ceda9a947273a7e1908f15464c54
Stovcikova <henrieta.stovcikova@kosickaakademia.sk>
2021-04-20 16:38:37 +0200

    add Header and Footer, vuetify

---
+++ Client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <Header/>
+    
+    <Footer/>
+    
+
+    <v-main>
+      
+      
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
+
+export default {
+  
+  name: 'App',
+
+  components: {
+    Footer, Header,
+   
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
